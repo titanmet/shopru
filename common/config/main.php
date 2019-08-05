c280ab9e74723198e3839dbc4a73d10f20db1a90
titanmet <titanmet@mail.ru>
2019-08-05 15:58:26 +0300

    init

--- common/config/main.php
+++ common/config/main.php
@@ -7,7 +7,8 @@ return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
         'cache' => [
-            'class' => 'yii\caching\FileCache',
+            'class' => 'yii\caching\MemCache',
+            'useMemcached' => true,
         ],
     ],
 ];
